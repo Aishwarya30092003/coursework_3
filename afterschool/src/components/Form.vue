fe67d4679062e350fde7e9c6a445a268fa399a66
Aishwarya Sharma <aishwarya.abss@gmail.com>
2024-03-30 16:04:18 +0400

    PlaceOrder

--- afterschool/src/components/Form.vue
+++ afterschool/src/components/Form.vue
@@ -7,11 +7,37 @@
       <button @click="removeItem(product.id)">remove</button>
     </div>
     <div>
-      <strong>Name</strong>
-      <input v-model="name" />
-      <strong> Address </strong>
-      <input v-model="address" />
-      <button @click="submitForm">Submit</button>
+      <div class="order-info" style="color: white; margin-top: 20px">
+        <h2>Order Information</h2>
+        <label for="firstName">First Name:</label>
+        <input
+          type="text"
+          id="firstName"
+          v-model="order.firstName"
+          @input="validateOrderForm"
+        />
+        <br /><br />
+        <label for="lastName">Last Name:</label>
+        <input
+          type="text"
+          id="lastName"
+          v-model="order.lastName"
+          @input="validateOrderForm"
+        />
+        <br /><br />
+        <label for="phone">Phone:</label>
+        <input
+          type="text"
+          id="phone"
+          v-model="order.phone"
+          @input="validateOrderForm"
+        />
+        <br /><br />
+        <!-- Button to place the order with conditional disabled state -->
+        <button @click="submitForm" :disabled="!isOrderFormValid">
+          Place Order
+        </button>
+      </div>
     </div>
   </div>
 </template>
@@ -22,8 +48,11 @@ export default {
   props: ["cart", "products"],
   data() {
     return {
-      name: "",
-      address: "",
+      order: {
+        firstName: "",
+        lastName: "",
+        phone: "",
+      },
     };
   },
 
@@ -34,9 +63,18 @@ export default {
       this.$emit("remove-item", index);
     },
     submitForm() {
-      console.log("Form submitted with", this.name, this.address);
-      this.name = "";
-      this.address = "";
+      alert("Order Submitted!");
+    },
+    validateOrderForm() {
+      const nameRegex = /^[A-Za-z]+$/;
+      const phoneRegex = /^[0-9]+$/;
+
+      const isNameValid =
+        nameRegex.test(this.order.firstName) &&
+        nameRegex.test(this.order.lastName);
+      const isPhoneValid = phoneRegex.test(this.order.phone);
+
+      this.isOrderFormValid = isNameValid && isPhoneValid;
     },
   },
 };
